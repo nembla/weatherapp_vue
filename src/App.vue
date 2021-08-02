77802c163904054a869c74b37552252faa090ed3
Nemanja Blagojevic <sixty@LOKI.local>
2021-08-02 14:49:32 -0400

    added button front to swap c and f, added icon to name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
+  <!-- google fonts include -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
     href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
     rel="stylesheet"
+  />
+
+  <!-- bootstrap include for icon -->
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
   />
 
   <div
@@ -15,7 +22,8 @@
   >
     <main>
       <div class="header-wrap">
-        <div class="header">(insert cool name)</div>
+        <div class="header">cloud.world <i class="bi bi-cloud-haze"></i></div>
+        <!-- On click make it go to my personal website -->
       </div>
       <div class="search-box">
         <input
@@ -37,7 +45,19 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="temp">
+            {{ Math.round(weather.main.temp) }}°c
+            <label class="switch">
+              <input class="switch-input" type="checkbox" />
+              <span
+                class="switch-label"
+                data-on="Imperial"
+                data-off="Metric"
+              ></span>
+              <span class="switch-handle"></span>
+            </label>
+          </div>
+
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
@@ -60,11 +80,6 @@ export default {
     };
   },
   methods: {
-    getPhotos(images) {
-      images.map((image) => {
-        console.log(image);
-      });
-    },
     bgImg() {
       fetch(`https://api.pexels.com/v1/search?query=${this.query}`, {
         headers: {
@@ -75,8 +90,7 @@ export default {
           return resp.json();
         })
         .then((data) => {
-          this.image = data.photos[0].src.landscape;
-          console.log(data.photos);
+          this.image = data.photos[0].src.large;
         });
     },
     fetchWeather() {
@@ -160,13 +174,11 @@ main {
   font-size: 25px;
   font-weight: 300;
   color: #fff;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
   margin-bottom: 50px;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.5);
   border-radius: 0px 16px;
   padding: 10px;
-  background-color: rgba(255, 255, 255, 0.15);
   font-family: "Bebas Neue", sans-serif;
+  text-shadow: 2px 3px rgba(0, 0, 0, 1);
 }
 
 .search-box {
@@ -250,5 +262,114 @@ main {
 .weather-box .temp:hover {
   background-color: rgba(0, 0, 0, 0.25);
   cursor: pointer;
+}
+
+.switch {
+  position: relative;
+  display: block;
+  width: 100px;
+  height: 30px;
+  padding: 3px;
+  margin-left: auto;
+  margin-right: auto;
+  background: linear-gradient(to bottom, #eeeeee, #ffffff 25px);
+  background-image: -webkit-linear-gradient(top, #eeeeee, #ffffff 25px);
+  border-radius: 18px;
+  box-shadow: inset 0 -1px white, inset 0 1px 1px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+  box-sizing: content-box;
+}
+.switch-input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  box-sizing: content-box;
+}
+.switch-label {
+  position: relative;
+  display: block;
+  height: inherit;
+  font-size: 10px;
+  text-transform: uppercase;
+  background: #eceeef;
+  border-radius: inherit;
+  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.12),
+    inset 0 0 2px rgba(0, 0, 0, 0.15);
+  box-sizing: content-box;
+}
+.switch-label:before,
+.switch-label:after {
+  position: absolute;
+  top: 50%;
+  margin-top: -0.5em;
+  line-height: 1;
+  -webkit-transition: inherit;
+  -moz-transition: inherit;
+  -o-transition: inherit;
+  transition: inherit;
+  box-sizing: content-box;
+}
+.switch-label:before {
+  content: attr(data-off);
+  right: 11px;
+  color: #aaaaaa;
+  text-shadow: 0 1px rgba(255, 255, 255, 0.5);
+}
+.switch-label:after {
+  content: attr(data-on);
+  left: 11px;
+  color: #ffffff;
+  text-shadow: 0 1px rgba(0, 0, 0, 0.2);
+  opacity: 0;
+}
+.switch-input:checked ~ .switch-label {
+  background: #2d6a4f;
+  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.15),
+    inset 0 0 3px rgba(0, 0, 0, 0.2);
+}
+.switch-input:checked ~ .switch-label:before {
+  opacity: 0;
+}
+.switch-input:checked ~ .switch-label:after {
+  opacity: 1;
+}
+.switch-handle {
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  width: 28px;
+  height: 28px;
+  background: linear-gradient(to bottom, #ffffff 40%, #f0f0f0);
+  background-image: -webkit-linear-gradient(top, #ffffff 40%, #f0f0f0);
+  border-radius: 100%;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+}
+.switch-handle:before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -6px 0 0 -6px;
+  width: 12px;
+  height: 12px;
+  background: linear-gradient(to bottom, #eeeeee, #ffffff);
+  background-image: -webkit-linear-gradient(top, #eeeeee, #ffffff);
+  border-radius: 6px;
+  box-shadow: inset 0 1px rgba(0, 0, 0, 0.02);
+}
+.switch-input:checked ~ .switch-handle {
+  left: 74px;
+  box-shadow: -1px 1px 5px rgba(0, 0, 0, 0.2);
+}
+
+/* Transition
+========================== */
+.switch-label,
+.switch-handle {
+  transition: All 0.3s ease;
+  -webkit-transition: All 0.3s ease;
+  -moz-transition: All 0.3s ease;
+  -o-transition: All 0.3s ease;
 }
 </style>
